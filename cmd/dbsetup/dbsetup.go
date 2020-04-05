34a4cfdf0a8c449fc75af82acd95edf49811b4fe
Johnny Lin <johnny.lin.prime@gmail.com>
2020-04-05 17:47:35 -0400

    Add cli for dbsetup (Can be used to test db queries)

--- cmd/dbsetup/dbsetup.go
+++ cmd/dbsetup/dbsetup.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
+	"os"
+	"strings"
 
 	"../../pkg/chatapi/database"
 	"github.com/joho/godotenv"
@@ -13,7 +17,33 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	database.Migrate()
+	startCLI()
+}
 
-	log.Println("Execution complete")
+func startCLI() {
+
+	var input string
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("dbmanag > ")
+
+		input, _ = reader.ReadString('\n')
+
+		input = strings.TrimSuffix(strings.TrimSuffix(input, "\n"), "\r")
+
+		switch input {
+		case "migrate":
+			database.Migrate()
+		case "getlast100messages":
+			messages := database.GetLast100Messages("#general")
+			for i, message := range messages {
+				log.Println(i, message.Timestamp, message.Message)
+			}
+		case "exit", "quit":
+			break
+		default:
+			fmt.Println("Command mismatch")
+		}
+
+	}
 }
