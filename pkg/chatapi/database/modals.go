c5d3c2472ace9ba07366c3863739653bbdfc6f75
Johnny Lin <johnny.lin.prime@gmail.com>
2020-04-05 16:41:55 -0400

    Update modals.go. Use UUIDv4 for PK instead of "gorm.Modal"

--- pkg/chatapi/database/modals.go
+++ pkg/chatapi/database/modals.go
@@ -3,15 +3,12 @@ package database
 import (
 	"time"
 
-	"github.com/jinzhu/gorm"
-
 	// blank import for postgres
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 // Chatroom struct for db
 type Chatroom struct {
-	gorm.Model
 	Name            string
 	Mood            string
 	LastMessage     string
@@ -20,6 +17,7 @@ type Chatroom struct {
 
 // Message struct for db
 type Message struct {
+	ID        string `gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
 	Chatroom  string
 	Timestamp time.Time
 	Sender    string
